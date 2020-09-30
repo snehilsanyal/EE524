0e11e40935e2c1ef9b8fe4117c8ffd09e3903a5c
Snehil Sanyal <41544905+snehilsanyal@users.noreply.github.com>
2020-09-30 14:05:01 +0530

    Delete asgnmnt 1-5.py

--- Assignment1/asgnmnt 1-5.py
+++
@@ -1,7 +0,0 @@
-#LCM of two number
-x=int(input("Please enter the 1st number:"))
-y=int(input("Please enter the 2nd number:"))
-for m in range(1, x*y+1):
-    if m%x==0 and m%y==0:
-        print("The HCF is",m)
-        break

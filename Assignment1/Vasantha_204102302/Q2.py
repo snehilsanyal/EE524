9911dde9b640772421d70434d8cade421d605f67
vasantha <71541257+vasantha3708@users.noreply.github.com>
2020-09-30 22:23:22 +0530

    Add files via upload

---
+++ Assignment1/Vasantha_204102302/Q2.py
@@ -0,0 +1,12 @@
+a=int(input('enter the value of a ='))
+b=int(input('enter the value of b ='))
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+g=a%b
+print('c=a+b=',c)
+print('d=a-b=',d)
+print('e=a*b=',e)
+print('f=a/b=',f)
+print('g=a%b=',g)
